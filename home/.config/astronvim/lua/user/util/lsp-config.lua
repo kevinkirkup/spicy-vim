09c986bb95a16dce41dcf2f8408b98673b14d6ed
Kevin S Kirkup <k.kirkup@digitalrealty.com>
2023-10-12 09:33:25 -0400

    Fix file formatting

--- home/.config/astronvim/lua/user/util/lsp-config.lua
+++ home/.config/astronvim/lua/user/util/lsp-config.lua
@@ -72,8 +72,8 @@ function M.custom_attach(client, bufnr)
 
 			local cursor_pos = api.nvim_win_get_cursor(0)
 			if
-					(cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
-					and #vim.diagnostic.get() > 0
+				(cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
+				and #vim.diagnostic.get() > 0
 			then
 				vim.diagnostic.open_float(nil, float_opts)
 			end
