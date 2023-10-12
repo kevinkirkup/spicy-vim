5226d2e1c0c38f9dbc27a5be55f0d7d51dbf0825
Kevin S Kirkup <k.kirkup@digitalrealty.com>
2023-10-12 10:05:49 -0400

    Move to indent-blankline v3 config

--- home/.config/astronvim/lua/user/plugins/indent-blankline.lua
+++ home/.config/astronvim/lua/user/plugins/indent-blankline.lua
@@ -1,30 +1,33 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
 	opts = {
-		-- U+2502 may also be a good choice, it will be on the middle of cursor.
-		-- U+250A is also a good choice
-		-- char = "▏",
-		space_char_blankline = " ",
-		show_current_context = true,
-		char_highlight_list = { "IndentBlanklineIndent" },
-		show_end_of_line = false,
-		disable_with_nolist = true,
-		buftype_exclude = { "terminal", "nofile" },
-		filetype_exclude = {
-			"help",
-			"alpha",
-			"dashboard",
-			"neo-tree",
-			"checkhealth",
-			"packer",
-			"lspinfo",
-			"git",
-			"gitconfig",
-			"markdown",
-			"snippets",
-			"text",
+		whitespace = {
+			remove_blankline_trail = true,
 		},
-		use_treesitter = false,
+		scope = {
+			show_start = true,
+			show_end = true,
+			include = {
+				node_type = {
+					["*"] = { "*" },
+				},
+			},
+		},
+		indent = {
+			smart_indent_cap = true,
+		},
+		exclude = {
+			filetypes = {
+				"checkhealth",
+				"packer",
+				"lspinfo",
+				"git",
+				"gitconfig",
+				"markdown",
+				"snippets",
+				"text",
+			},
+		},
 	},
-	config = require("user.plugins.configs.indent-blankline"),
 }
