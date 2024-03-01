715613c7227c12fdfbd82ce76b264b6d2772ffba
Kevin S Kirkup <kevin.kirkup@gmail.com>
2024-03-01 09:21:26 -0500

    Fix healthchecks

--- home/.config/astronvim/lua/user/options.lua
+++ home/.config/astronvim/lua/user/options.lua
@@ -5,10 +5,12 @@ return {
 		termguicolors = true,
 		foldmethod = "expr",
 		foldexpr = "nvim_treesitter#foldexpr()",
+		conceallevel = 2,
 	},
 	g = {
 		-- logging_level = "info",
-		python3_host_prog = "/opt/homebrew/bin/python3",
+		loaded_perl_provider = 0,
+		python3_host_prog = "/opt/homebrew/anaconda3/bin/python3",
 		neoformat_only_msg_on_error = 1,
 		neo_tree_remove_legacy_commands = true,
 	},
